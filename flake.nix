692027d35d843f814f4a85fcef35efef695fe316
Zach Rumancik <zach.rumancik@scscloud.com>
2026-03-07 13:51:17 -0500

    Fix formatting error with flake-parts

--- flake.nix
+++ flake.nix
@@ -6,12 +6,13 @@
     flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
-  outputs =
-    inputs@{ self, nixpkgs, disko, flake-parts, ... }:
+  outputs = inputs@{ self, nixpkgs, disko, flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [ "x86_64-linux" ];
-  
+
       perSystem = { pkgs, ... }: {
+        _module.args.pkgs = pkgs;
+        
         nixosConfigurations = {
           hab-lab = nixpkgs.lib.nixosSystem {
             system = "x86_64-linux";
@@ -24,37 +25,10 @@
           warframe = nixpkgs.lib.nixosSystem {
             system = "x86_64-linux";
             modules = [
-              disko.nixosModules.disko
               ./hosts/warframe/configuration.nix
             ];
           };
         };
       };
-  
-      # Example: Future hosts can be added here
-      # perSystem.hab-atlas = { pkgs, ... }: {
-      #   nixosConfigurations = {
-      #     hab-atlas = pkgs.nixos {
-      #       configuration = {
-      #         imports = [
-      #           ./hosts/hab-atlas/configuration.nix
-      #           disko.nixosModules.disko
-      #         ];
-      #       };
-      #     };
-      #   };
-      # };
-  
-      # perSystem.pikvm = { pkgs, ... }: {
-      #   nixosConfigurations = {
-      #     pikvm = pkgs.nixos {
-      #       configuration = {
-      #         imports = [
-      #           ./hosts/pikvm/configuration.nix
-      #         ];
-      #       };
-      #     };
-      #   };
-      # };
     };
 }
