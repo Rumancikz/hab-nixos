9bcb42c744ccab2974506dc00d59ee7314c7dcc8
zman <rumancik.zach@gmail.com>
2026-03-07 16:16:09 -0500

    Remove input options

--- modules/input/default.nix
+++ modules/input/default.nix
@@ -4,14 +4,6 @@
   # Input device configuration
   services.xserver.libinput.enable = true;
 
-  # Touchpad settings for laptop
-  services.xserver.libinput.touchpad = {
-    tapping = true;
-    naturalScrolling = true;
-    twoFingerScrolling = true;
-    clickMethod = "fingers";
-  };
-
   # Keyboard settings
   services.xserver.xkb = {
     layout = "us";
