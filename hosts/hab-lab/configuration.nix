04a264d0cee5087077a276da2c4f995090727d8f
zman <rumancik.zach@gmail.com>
2026-03-12 21:58:26 -0400

    allow new port

--- hosts/hab-lab/configuration.nix
+++ hosts/hab-lab/configuration.nix
@@ -35,7 +35,7 @@
       enable = true;
       allowedTCPPorts = [
         8080
-        28981
+        3343
         # config.services.firefly-iii.settings.DB_PORT
         config.services.mealie.port
       ];
