2a8c7015231b020fed077f93f20057ff34c9c517
zman <rumancik.zach@gmail.com>
2026-03-14 10:58:44 -0400

    Fix spelling

--- modules/services/nextcloud.nix
+++ modules/services/nextcloud.nix
@@ -24,7 +24,7 @@
 
       # Database and Caching
       database.createLocally = true;
-      configureRedis = treu;
+      configureRedis = true;
       maxUploadSize = "16G";
 
       autoUpdateApps.enable = true;
